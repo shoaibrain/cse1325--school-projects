8f4e23ebb080b359c19f5a486767cc0bd09cbde8
Shoaib Rain <rainshoaib2013@outlook.com>
2021-02-02 06:55:12 -0600

    Final Submission

--- P02/full_credit/pet.h
+++ P02/full_credit/pet.h
@@ -5,28 +5,24 @@
 
 #include <iostream>
 #include <string>
-#include <exception>
 
 using namespace std;
 enum Type {DOG, CAT, HAMSTER, SNAKE,TIGER};
 
 class Pet{
     public:
-        Pet(string name, double age,Type type);     //Constructor
+        Pet(string _name, double _age,Type _type);     //Constructor
  
         ~Pet();                                      //Destructor
+
+    std::string to_string();     //to_string method
         
     private:
        string _name;
        double _age;
        Type _type;
 
-    //to_string method
-    string to_string(){
 
-    string summary = _name+" is a " + std::to_string(_type) + " age "+std::to_string(_age);
-    return summary;
-};
 
 };
    
