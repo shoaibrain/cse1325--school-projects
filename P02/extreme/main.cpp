6312c4575c2a079a5893ad3ec97e73d20ac4740f
Shoaib Rain <rainshoaib2013@outlook.com>
2021-02-04 10:55:34 -0600

    Extreme bonus added

--- P02/extreme/main.cpp
+++ P02/extreme/main.cpp
@@ -2,7 +2,11 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <algorithm>
 
+bool less_than(Pet p1, Pet p2){
+	return p1.pet_years() < p2.pet_years();
+}
 
 int main(){
 
@@ -11,7 +15,7 @@ int main(){
 		Pet{"Corey",2,DOG},
 		Pet{"Jackie",1,HAMSTER},
 	};
-
+	std::sort(pets.begin(), pets.end(), less_than);
     for (Pet& p:pets) std::cout <<p.to_string() << std::endl;
 
 }
---
+++ P02/extreme/pet.h
@@ -0,0 +1,35 @@
+#ifndef __PET_H
+#define __PET_H
+
+
+#include <vector>
+#include <string>
+
+enum Type {DOG, CAT, HAMSTER};
+//Create a vector to have a string representation of enum Type
+const std::vector<std::string> to_string{
+	"dog", "cat", "hamster"
+};
+
+const double human_lifespan = 80.0;
+const std::vector<double> pet_lifespan{
+	10.0,14.0,27.0,
+};
+
+class Pet{
+    public:
+        Pet(std::string _name, double age,Type type);     //Constructor
+		std::string to_string();     //to_string method
+		double pet_years();
+		
+        
+    private:
+       std::string _name;
+       double _age;
+       Type _type;
+
+
+
+};
+   
+#endif
