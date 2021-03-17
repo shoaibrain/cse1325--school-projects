204da4d6b266e5dcfb0c6d339599e64c391ccd3a
Shoaib Rain <rainshoaib2013@outlook.com>
2021-03-17 12:46:36 -0500

    mainwin: draft2

--- P06/Sprint2/mainwin.cpp
+++ P06/Sprint2/mainwin.cpp
@@ -21,10 +21,53 @@ Mainwin::Mainwin() {
     // Add a menu bar as the top item in the vertical box
     Gtk::MenuBar *menubar = Gtk::manage(new Gtk::MenuBar);
     vbox->pack_start(*menubar, Gtk::PACK_SHRINK, 0);
-    // vbox->add(*menubar);
+    //vbox->add(*menubar);
+	
+    //     F I L E
+    // Create a File menu and add to the menu bar
+    Gtk::MenuItem *menuitem_file = Gtk::manage(new Gtk::MenuItem("_File", true));
+    menubar->append(*menuitem_file);
+    Gtk::Menu *filemenu = Gtk::manage(new Gtk::Menu());
+    menuitem_file->set_submenu(*filemenu);
+
+    //         N E W   G A M E
+    // Append New to the File menu
+    Gtk::MenuItem *menuitem_new = Gtk::manage(new Gtk::MenuItem("_New School", true));
+    menuitem_new->signal_activate().connect([this] {this->on_new_parent_click();});
+    filemenu->append(*menuitem_new);
+
+    //         Q U I T
+    // Append Quit to the File menu
+    Gtk::MenuItem *menuitem_quit = Gtk::manage(new Gtk::MenuItem("_Quit", true));
+    menuitem_quit->signal_activate().connect([this] {this->on_quit_click();});
+    filemenu->append(*menuitem_quit);
 
 
 
+}
+Mainwin::~Mainwin(){}
 
+//CALL BACKS
 
+void Mainwin::on_new_school_click(){
+	//do something
+	std::cout<<"";
+	
+}
+
+void Mainwin::on_new_student_click(){
+	//do something
+}
+
+void Mainwin::on_new_parent_click(){
+	//do something
+}
+
+void Mainwin::on_student_parent_click(){
+	//do something
+}
+
+void Mainwin::on_quit_click(){
+	//do something
+	close();
 }
