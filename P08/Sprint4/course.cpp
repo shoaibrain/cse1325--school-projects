61bf63f9aaf0fc67a825bdcf7fb95437e8c2d8b4
Shoaib Rain <rainshoaib2013@outlook.com>
2021-04-12 19:15:49 -0500

    course class compiles

--- P08/Sprint4/course.cpp
+++ P08/Sprint4/course.cpp
@@ -1,16 +1,20 @@
 #include "course.h"
+#include "subject.h"
 #include <iostream>
 Course::Course(Subject subject, int grade)
 	: _subject{subject}, _grade{grade}{}
 	
 Course::Course(std::istream& ist){
-	
-	//_subject = new Subject(ist);
-	//ist >> _grade;
+	std::string subject;
+	std::getline(ist,subject);
+	ist >> _grade;
+	ist.ignore(32767, '\n');
+
+	_subject = string_to_subject(subject);
 }
 void Course::save(std::ostream &ost){
 	//Todo
-	ost << _subject << '\n' << _grade << '\n';
+	ost << subject_to_string(_subject) << '\n' << _grade << '\n';
 }
 
 std::ostream &operator<<(std::ostream &ost, const Course &course){
--- P08/Sprint4/section.cpp
+++ P08/Sprint4/section.cpp
@@ -1,25 +1,28 @@
 #include "section.h"
+#include "course.h"
+#include "semester.h"
+#include "subject.h"
 #include <iostream>
 
 Section::Section(Course course, Semester semester, int year )
     : _course{course}, _semester{semester}, _year{year} {}
 
-/*
-Section::Section(std::istream& ist)
-{
-    //Todo
-	//std::getline(ist,_course);
-	//std::getline(ist, _semester);
-	//ist >> _year;
-	//ist.ignore(32767, '\n');
+
+Section::Section(std::istream& ist){    
+	_course(ist);
+	
+	load_semester(ist);
+	
+	ist >> _year;
+	ist.ignore(32767, '\n');
+	
 }
-*/
+
 void Section::save(std::ostream &ost)
 {
-    //need some work
-    ost << _course << '\n'
-        << _semester << '\n'
-        << _year << '\n';
+	 _course.save(ost);
+	 ost << _semester << "\n" << _year << "\n";
+	 
 }
 std::ostream &operator<<(std::ostream &ost, const Section &section)
 {
