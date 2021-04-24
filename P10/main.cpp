f5cb447d1084b172555ca9a05b5d393e76ffe485
Shoaib Rain <rainshoaib2013@outlook.com>
2021-04-24 13:21:07 -0500

    helper func added

--- P10/main.cpp
+++ P10/main.cpp
@@ -6,9 +6,19 @@
 #include <vector>
 
 #include <algorithm>  // std::sort
+#include <thread>
+#include <mutex>
+
+void helper(std::vector<Solution> &S_vector,std::string word, Puzzle puzzle){
+	Solver solver{puzzle};
+	Solution s = solver.solve(word);
+	S_vector.push_back(s);
+	
+}
 
 int main(int argc, char* argv[]) {
 
+   //int numPuzzle = 
     // Load the puzzles
     std::vector<Puzzle> puzzles;
     for(int i=1; i<argc; ++i) {
@@ -30,18 +40,31 @@ int main(int argc, char* argv[]) {
     // Capture the solutions in a vector 
     std::vector<Solution> solutions;
 
+     int nthreads = 4;
+	 int for_each_thread = puzzles.size()/nthreads;
+
     // Loop through all of the loaded puzzles
     for(Puzzle& puzzle : puzzles) {
-
+         
         // Find each word in the current puzzle
         for(std::string word : puzzle) {
         
             // Instance a Solver, and use it to solve for the word
-            Solver solver{puzzle};
-            Solution s = solver.solve(word);
-            solutions.push_back(s);
+            //Solver solver{puzzle};
+            //Solution s;
+//			= solver.solve(word);
+			
+			//std::thread t{&Solver::solve, &solver, word};
+            //t.join();
+			//solutions.push_back(s);
+			
+			std::thread t{helper,&solutions,word,puzzle};
+			
+			
+			
         }
     }
+	
     
     std::sort(solutions.begin(), solutions.end());
     for(Solution& s : solutions) std::cout << s << std::endl;
